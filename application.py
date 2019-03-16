8a01523049914c5b6543bebf53829c9943b38f46
Sparsh Gupta <sparshgupta318@gmail.com>
2019-03-16 10:46:25 +0530

    Added review part and Api

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 import requests
-from flask import Flask, session,render_template,url_for,request
+from flask import Flask, session,render_template,url_for,request,jsonify
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -33,6 +33,7 @@ def login():
     if(request.method=="POST"):
         if(not (request.form.get("username")) or not (request.form.get("password"))):
             return "Apology nothing shoud be none"
+        session["user"]=request.form.get("username")
         user=db.execute("SELECT * FROM users where username=:username",{"username":request.form.get("username")}).fetchone()
         if not user:
             return "username not found"
@@ -85,9 +86,26 @@ def search():
     else:
        return  render_template("search.html")
 
-@app.route("/book/<string:isbn>")
+@app.route("/book/<string:isbn>",methods=["GET","POST"])
 def books(isbn):
+    if(request.method=="POST"):
+        db.execute("Insert into reviews(id,review,rating,isbn)VALUES(:id,:review,:rating,:isbn)",{"id":session["user"],"review":request.form.get("review"),"isbn":isbn,"rating":request.form.get("rating")})
+        db.commit()
+    result=db.execute("SELECT * FROM books where isbn=:isbn",{"isbn":isbn}).fetchone()
+    print(session["user"])
+    res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "XGwRVb91C7ZSmbwNetIg", "isbns": isbn}).json()
+    review=db.execute("SELECT review,rating from reviews where isbn=:isbn",{"isbn":isbn}).fetchall()
+    return  render_template("books.html",result=result,res=res,review=review)
+
+@app.route("/api/<string:isbn>")
+def api(isbn):
     result=db.execute("SELECT * FROM books where isbn=:isbn",{"isbn":isbn}).fetchone()
     res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "XGwRVb91C7ZSmbwNetIg", "isbns": isbn}).json()
-    
-    return  render_template("books.html",result=result,res=res)
+    return jsonify(
+            title=result.title,
+            author=result.author,
+            year=result.year,
+            isbn=result.isbn,
+            review_count=res["books"][0]["work_ratings_count"],
+            average_score=res["books"][0]["average_rating"]
+    )
