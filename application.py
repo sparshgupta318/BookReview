b77fcbea4b65a18f1b72fe1d20cfd904486188e6
Sparsh Gupta <sparshgupta318@gmail.com>
2019-03-17 20:03:00 +0530

    Final touch

--- application.py
+++ application.py
@@ -8,6 +8,7 @@ from werkzeug.security import generate_password_hash,check_password_hash
 
 
 app = Flask(__name__)
+app.secret_key = os.urandom(16)
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
@@ -16,14 +17,12 @@ if not os.getenv("DATABASE_URL"):
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-
 Session(app)
 
 # Set up database
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-app.config["SECRET_KEY"]="geajknejqn"
 
 @app.route("/")
 def index():
@@ -31,12 +30,17 @@ def index():
 
 @app.route("/login",methods=["GET","POST"])
 def login():
-    if(request.method=="POST"):
+    if 'user' in session:
+        return redirect(url_for("search"))
+    elif(request.method=="POST"):
         if(not (request.form.get("username")) or not (request.form.get("password"))):
-            return "Apology nothing shoud be none"
+            error="Apology nothing shoud be none"
+            return render_template("error.html",error=error)
         user=db.execute("SELECT * FROM users where username=:username",{"username":request.form.get("username")}).fetchone()
         if not user:
-            return "username not found"
+             error="username not found"
+             return render_template("error.html",error=error)
+ 
     
         hash=db.execute("SELECT password FROM users WHERE username=:username",{"username":request.form.get("username")}).fetchone()
         
@@ -44,10 +48,12 @@ def login():
             session["user"]=request.form.get("username")
             return redirect(url_for("search")) 
         else:
-            return "wrong password"   
+            error= "wrong password"
+            return render_template("error.html",error=error)
+   
             
     else:
-          return render_template("login.html",session=session)
+          return render_template("login.html")
     
 
 @app.route("/logout")
@@ -58,19 +64,24 @@ def logout():
 
 @app.route("/register",methods=["GET","POST"])
 def register():
-    if user in session:
+    if 'user' in session:
         return redirect(url_for("search"))
   
     elif(request.method=="POST"):
         if(not (request.form.get("username")) or not (request.form.get("password")) or not (request.form.get("cpassword"))):
-            return "Apology nothing shoud be none"
+            error="Apology nothing shoud be none"
+            return render_template("error.html",error=error)
         elif(not request.form.get("password")==request.form.get("cpassword")):
-            return "passwords not same"
+            error= "passwords not same"
+            return render_template("error.html",error=error)
+
         hash=generate_password_hash(request.form.get("password"))
 
         user=db.execute("SELECT * FROM users where username=:username",{"username":request.form.get("username")}).fetchone()
         if user:
-            return "duplicate"
+            error= "duplicate"
+            return render_template("error.html",error=error)
+
         else:
             session["user"]=request.form.get("username")
             db.execute("INSERT INTO users(username,password)VALUES(:user,:hash)",{"user":request.form.get("username"),"hash":hash})
@@ -79,27 +90,30 @@ def register():
             return redirect(url_for("search"))    
             
     else:
-          return render_template("register.html",session=session)
+       
+          return render_template("register.html")
 
 
     
 
 @app.route("/search",methods=["GET","POST"])
 def search():
-     if user not in session:
+     if 'user' not in session:
              return redirect(url_for("login"))
      elif(request.method=="POST"):
             result=db.execute("SELECT * FROM books where isbn LIKE :q or title LIKE :q or author LIKE :q or year LIKE :q",{"q":'%'+request.form.get("search")+'%'}).fetchall()
             if result is None:
-                return "NO MATCHES FOUND"
+                error= "NO MATCHES FOUND"
+                return render_template("error.html",error=error)
+
             else:
                 return render_template("search.html",result=result)
      else:
-             return  render_template("search.html",session=session)
+             return  render_template("search.html")
 
 @app.route("/book/<string:isbn>",methods=["GET","POST"])
 def books(isbn):
-    if user not in session:
+    if 'user' not in session:
         return redirect(url_for("login"))
     elif(request.method=="POST"):
         count=db.execute("SELECT * FROM reviews where id=:id and isbn=:isbn",{"id":session["user"],"isbn":isbn}).fetchone()
@@ -110,7 +124,7 @@ def books(isbn):
     print(session["user"])
     res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "XGwRVb91C7ZSmbwNetIg", "isbns": isbn}).json()
     review=db.execute("SELECT review,rating from reviews where isbn=:isbn",{"isbn":isbn}).fetchall()
-    return  render_template("books.html",result=result,res=res,review=review,session=session)
+    return  render_template("books.html",result=result,res=res,review=review)
 
 @app.route("/api/<string:isbn>")
 def api(isbn):
